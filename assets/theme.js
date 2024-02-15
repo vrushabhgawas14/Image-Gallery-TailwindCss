83ddfe0b8800b65bcbe7ccfd089519f848e82702
Vrushabh Gawas <vrushabhgawas14@gmail.com>
2024-02-15 18:15:39 +0530

    Added tailwind darkmode

--- assets/theme.js
+++ assets/theme.js
@@ -1,1 +1,9 @@
 let theme = document.getElementById('theme');
+
+theme.addEventListener('click',()=>{
+    if(document.documentElement.classList.value =='dark'){
+        document.documentElement.classList.remove('dark');
+    }else{
+        document.documentElement.classList.add('dark');
+    }
+})
