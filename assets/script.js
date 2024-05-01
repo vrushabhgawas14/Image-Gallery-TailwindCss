41134c3d3402ceaa8d170564d2bb195b060d5de7
Vrushabh <vrushabhgawas14@gmail.com>
2024-05-01 12:45:54 +0530

    Update code to effcient

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,23 @@
-let backBtn = document.getElementById('backBtn');
-let nextBtn = document.getElementById('nextBtn');
-let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+let scrollContainer = document.querySelector("#gallery");
 
-scrollContainer.addEventListener('wheel',(e) =>{
-    e.preventDefault();
-    scrollContainer.style.scrollBehavior = 'auto';
-    scrollContainer.scrollLeft += e.deltaY;
+scrollContainer.addEventListener("wheel", (e) => {
+  e.preventDefault();
+  scrollContainer.style.scrollBehavior = "auto";
+  scrollContainer.scrollLeft += e.deltaY;
 });
 
-backBtn.addEventListener('click',goLeft);
+backBtn.addEventListener("click", goLeft);
 
-nextBtn.addEventListener('click',goRight);
+nextBtn.addEventListener("click", goRight);
 
-function goLeft(){
-    scrollContainer.scrollLeft -= 1200;
-    scrollContainer.style.scrollBehavior = "smooth";
-};
+function goLeft() {
+  scrollContainer.scrollLeft -= 1200;
+  scrollContainer.style.scrollBehavior = "smooth";
+}
 
-function goRight(){
-    scrollContainer.scrollLeft += 1200;
-    scrollContainer.style.scrollBehavior = "smooth";
-};
+function goRight() {
+  scrollContainer.scrollLeft += 1200;
+  scrollContainer.style.scrollBehavior = "smooth";
+}
